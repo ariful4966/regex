5a29265f7f4232b7ea4cae8cbd109ef5e2d0ff5f
ariful4082 <ariful4082@gmail.com>
2021-11-12 08:16:14 +0600

    email validation check

--- regex.js
+++ regex.js
@@ -4,8 +4,12 @@
 // const regex = /#?([\da-fA-f]{2})([\da-fA-f]{2})([\da-fA-f]{2})/g;
 
 //Phone Number Validation
-const text = "+8801863934966"; //+88-01863934966
-const regex = /(\+88)?-?01[1-9]\d{8}/g;
+// const text = "+8801863934966"; //+88-01863934966
+// const regex = /(\+88)?-?01[1-9]\d{8}/g;
+
+//email validation
+const text = "ariful4966@gmail.com"; //ariful4966@gmail.com
+const regex = /\S+[a-z0-9]\@\S+[a-z]\.\S+[a-z]/gi;
 
 const matches = text.match(regex);
 
